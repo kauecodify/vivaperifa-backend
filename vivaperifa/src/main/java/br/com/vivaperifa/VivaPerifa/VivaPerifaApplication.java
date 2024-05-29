db18aab29882ef850128ef95eddb38c79874c30f
Raquel <raquelmoreira847@gmail.com>
2024-05-28 21:16:06 -0300

    initial commit

---
+++ vivaperifa/src/main/java/br/com/vivaperifa/VivaPerifa/VivaPerifaApplication.java
@@ -0,0 +1,13 @@
+package br.com.vivaperifa.VivaPerifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaPerifaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaPerifaApplication.class, args);
+	}
+
+}
